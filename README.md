358af8080715b05f0c9838232b2799fb74fe04b4
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-26 19:53:18 -0300

    Se agregaron las resoluciones de los apartados a,b,c,d y e en el archivo tp1_1.c y se agrego el archivo README.md con las respuestas a las preguntas del punto 1.

---
+++ README.md
@@ -0,0 +1,13 @@
+## Obersvaciones sobre los apartados b y c
+
+**El resultado es el mismo en ambos apartados**
+
+_i) ¿A qué se debe?_
+
+En el apartado 'c' se muestra la dirección de memoria de la variable 'number', la cual es la dirección a la cual apunta el puntero 'pointer_to_number'. Es por eso que coinciden los resultados obtenidos en ambos apartados.
+
+_ii) ¿Qué se obtiene en el apartado 'd'? ¿Es igual a las anteriores? ¿Por qué?_
+
+En el apartado 'd' se obtiene la dirección de memoria del puntero.
+
+Esta no es igual a las anteriores. Y esto se debe a que, el puntero es una variable más, la cual tiene su propia dirección de memoria. Diferente a la de su contenido (dirección de memoria de la variable a la que apunta).
--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,25 @@
 
 int main()
 {
+    int number = 25, *pointer_to_number = NULL;
+    pointer_to_number = &number;
+
+    //a: Contenido del puntero
+    printf("Contenido del puntero: %p", pointer_to_number);
+    printf("\nValor referenciado por el puntero: %d", *pointer_to_number);
+
+    //b: Dirección de memoria almacenada por el puntero
+    printf("\nDirección de memoria almacenada por el puntero: %p", pointer_to_number);
+
+    //c: Dirección de memoria de la variable
+    printf("\nDirección de memoria de la variable 'number': %p", &number);
+
+    //d: Dirección de memoria del puntero
+    printf("\nDirección de memoria del puntero: %p", &pointer_to_number);
+
+    //e: Tamaño de memoria utilizado por esa variable usando la función sizeof
+    printf("\nTamaño de la memoria utilizada por la variable, de tipo int, 'number': %d bytes", sizeof(number));
+    printf("\nTamaño de la memoria utilizada por la variable, de tipo puntero, 'pointer_to_number': %d bytes", sizeof(pointer_to_number));
+
     return 0;
 }
