bc83ddda390b18020b1e09237e8d02a10c4eda77
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-26 22:39:42 -0300

    Se trabajo sobre la función para calcular la potencia de un numero real. Se debe continuar trabajando en la misma para el caso de exponentes negativos.

--- tp1_2.c
+++ tp1_2.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
+float powerFunction(float base, int exponent);
+
 int main()
 {
+    float base;
+    int exponent;
+    printf("***Programa para calcular la potencia de un número real elevado a un exponente entero***");
+    printf("\nIngrese la base: ");
+    scanf("%f", &base);
+    printf("\nIngrese el exponente: ");
+    scanf("%d", &exponent);
+
+    printf("\nResultado de la operación: %.2f", powerFunction(base, exponent));
 
     return 0;
+}
+
+float powerFunction(float base, int exponent)
+{
+    float result = base;
+    int count = 1;
+    if (exponent == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        while (count < exponent)
+        {
+            result *= base;
+            count++;
+        }
+        if (exponent > 0)
+        {
+            return result;
+        }
+        else
+        {
+            return 1 / result;
+        }
+    }
 }
