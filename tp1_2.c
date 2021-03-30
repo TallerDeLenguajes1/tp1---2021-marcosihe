f759f62671f6f93650db0ab6545ab186c41f5325
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-30 10:47:55 -0300

    Se agrego la libreria stdlib.h en tp1_1.c para que tenga sentido el uso de la constante NULL. Se agrego la funcion pedida en el apartado c en tp1_2.c la cual muestra la direccion de memoria y contenido de una variable de tipo int ingresada por pantalla.

--- tp1_2.c
+++ tp1_2.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 float powerFunction(float base, int exponent);
 void powerFunction_2(float base, int exponent);
+void variable_data(int *pointer_to_random_int);
 
 int main()
 {
     float base;
-    int exponent;
-    printf("***Programa para calcular la potencia de un número real elevado a un exponente entero***");
+    int exponent, random_int, *pointer_to_random_int = NULL;
+
+    printf("*** Programa que muestra la dirección y el contenido de un entero ingresado por pantalla ***");
+    printf("\nIngrese un número entero: ");
+    scanf("%d", &random_int);
+    pointer_to_random_int = &random_int;
+
+    /* Apartado C*/
+    variable_data(pointer_to_random_int);
+
+    /* Apartado A*/
+    printf("\n***Programa para calcular la potencia de un número real elevado a un exponente entero***");
     printf("\nIngrese la base: ");
     scanf("%f", &base);
     printf("\nIngrese el exponente: ");
     scanf("%d", &exponent);
     printf("\nResultado de la operación: %.2f", powerFunction(base, exponent));
 
+    /* Apartado B*/
     printf("\n***Programa Nro 2 para calcular la potencia de un número real elevado a un exponente entero***");
     printf("\nIngrese la base: ");
     scanf("%f", &base);
@@ -78,3 +91,10 @@ void powerFunction_2(float base, int exponent)
         }
     }
 }
+
+void variable_data(int *pointer_to_random_int)
+{
+    printf("\nDirección de memoria de la variable ingresada: %p", pointer_to_random_int);
+    printf("\nContenido de la variable ingresada: %d", *pointer_to_random_int);
+}
+/**/
