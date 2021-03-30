cd6487030afb4f612a4bc48d840b007f6c30e0e1
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-30 09:54:26 -0300

    Se replico la funcion powerFunction pero modificando para que se sea de tipo void.

--- tp1_2.c
+++ tp1_2.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 float powerFunction(float base, int exponent);
+void powerFunction_2(float base, int exponent);
 
 int main()
 {
@@ -12,8 +13,14 @@ int main()
     scanf("%f", &base);
     printf("\nIngrese el exponente: ");
     scanf("%d", &exponent);
-
     printf("\nResultado de la operación: %.2f", powerFunction(base, exponent));
+
+    printf("\n***Programa Nro 2 para calcular la potencia de un número real elevado a un exponente entero***");
+    printf("\nIngrese la base: ");
+    scanf("%f", &base);
+    printf("\nIngrese el exponente: ");
+    scanf("%d", &exponent);
+    powerFunction_2(base, exponent);
 
     return 0;
 }
@@ -41,6 +48,33 @@ float powerFunction(float base, int exponent)
         else
         {
             return (float)(1 / result);
+        }
+    }
+}
+
+void powerFunction_2(float base, int exponent)
+{
+    float result = base;
+    int count = 1;
+    int aux = abs(exponent);
+    if (exponent == 0)
+    {
+        printf("%.2f elevado a %d es igual a: 1", base, exponent);
+    }
+    else
+    {
+        while (count < aux)
+        {
+            result *= base;
+            count++;
+        }
+        if (exponent > 0)
+        {
+            printf("%.2f elevado a %d es igual a: %.2f", base, exponent, result);
+        }
+        else
+        {
+            printf("%.2f elevado a %d es igual a: %.2f", base, exponent, (float)(1 / result));
         }
     }
 }
